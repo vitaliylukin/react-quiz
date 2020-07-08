be3b444bb50ce01a05c9800d10f944b776cfdd5d
@vitaliylukin91 <vitaliylukin91@gmail.com>
2020-07-08 18:21:52 +0300

    start practice forms

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -39,9 +39,9 @@ const FinishedQuiz = props => {
             <p>Правильно {successCount} из {props.quiz.length}</p>
 
             <div>
-                <Button onClick={props.onRetry} type='primary'>Повторить</Button>
-                <Link to={'/'}>
-                    <Button type='success'>Перейти в список тестов</Button>
+                <Button onClick={props.onRetry} type="primary">Повторить</Button>
+                <Link to="/">
+                    <Button type="success">Перейти в список тестов</Button>
                 </Link>
             </div>
         </div>
